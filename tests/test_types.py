6efae60ec5807649bb03d8ac2116ecdc8badacfd
Timtaran <tentaranka@gmail.com>
2024-07-09 00:16:46 +0500

    added simple types test

---
+++ tests/test_types.py
@@ -0,0 +1,13 @@
+from vts_api.types import * # noqa
+
+
+class TestTypes:
+    def test_types(self):
+        # idk how to write tests for types (not only)
+        # so I just do same what vkbottle/vkbottle-types does
+        # just test does it import all types
+        pass
+
+
+if __name__ == "__main__":
+    TestTypes().test_types()
